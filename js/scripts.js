9fbfa0ed583c1b7a9a1bf59b17b23f2c0685414d
Ian Baum and Rebecca Allen <rebecca@>
2016-08-10 10:38:52 -0400

    add buttons and add an add function to the calc

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,36 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
 $(document).ready(function(){
   $("#blanks form").submit(function(event){
+    event.preventDefault();
+
     var name = $("input#name").val();
 
     $(".letterName").text(name);
 
     $("#letter").show();
-
-    event.preventDefault();
   });
+
+  $("form#calculator").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    
+    alert(add(number1, number2));
+  });
+
 });
