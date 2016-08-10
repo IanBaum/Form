82bd34dbfc8ed3f929d291bed6812b9bfb6268a9
Ian Baum and Rebecca Allen <ian@>
2016-08-10 11:21:57 -0400

    add functionality to the calculator

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,27 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
+var action = 0;
+
+
 $(document).ready(function(){
+
+  $("#add").click(function(){
+    action = 0;
+  });
+
+  $("#subtract").click(function(){
+    action = 1;
+  });
+
+  $("#multiply").click(function(){
+    action = 2;
+  });
+
+  $("#divide").click(function(){
+    action = 3;
+  });
+
   $("#blanks form").submit(function(event){
     event.preventDefault();
 
@@ -29,8 +49,15 @@ $(document).ready(function(){
     event.preventDefault();
     var number1 = parseInt($("#input1").val());
     var number2 = parseInt($("#input2").val());
-    
-    alert(add(number1, number2));
+    if(action === 0){
+      $("#output").text(add(number1, number2));
+    }else if(action === 1){
+      $("#output").text(subtract(number1,number2));
+    }else if(action === 2){
+      $("#output").text(multiply(number1,number2));
+    }else{
+      $("#output").text(divide(number1,number2));
+    }
   });
 
 });
